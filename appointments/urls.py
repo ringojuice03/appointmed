bef825194ba29f7be994097558af34fbaace19d2
ringojuice03 <kanecorts@gmail.com>
2024-09-17 20:03:10 +0800

    template html linked to static css

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.say_hello),
-    path('patient/', views.patient_home),
+    path('patient1/', views.patient_home1),
+    path('patient2/', views.patient_home2),
     path('doctor/', views.doctor_home),
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 def say_hello(request):
     return render(request, 'hello.html')
 
-def patient_home(request):
-    return render(request, 'patient_home.html')
+def patient_home1(request):
+    return render(request, 'patient_home1.html')
+
+def patient_home2(request):
+    return render(request, 'patient_home2.html')
 
 def doctor_home(request):
-    return render(request, 'doctor_home.html')
+    return render(request, 'doctor_view.html')
