1181b9e954d8db2ac6934667b5eb4caba7aa1e11
ringojuice03 <kanecorts@gmail.com>
2024-09-17 14:39:51 +0800

    added templates

--- appointmed/urls.py
+++ appointmed/urls.py
@@ -20,5 +20,5 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('appointments.urls')),
+    path('', include('appointments.urls')),
 ] + debug_toolbar_urls()
--- appointments/urls.py
+++ appointments/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('patient/', views.say_hello),
+    path('', views.say_hello),
+    path('patient/', views.patient_home),
+    path('doctor/', views.doctor_home),
 ]
