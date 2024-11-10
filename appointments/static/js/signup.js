853f1b5e657802fbaa05306edd00d5210af07b58
hbubante <hbubante@gbox.adnu.edu.ph>
2024-11-10 15:47:59 +0800

    updated login and signup

--- appointments/static/js/signup.js
+++ appointments/static/js/signup.js
@@ -1,24 +1,17 @@
-const medicalLicenseField = document.getElementById("medical-license-field-cluster");
-const specialtyField = document.getElementById("specialty-field-cluster");
-const clinicAddressField = document.getElementById("clinic-address-field");
-
-var roleCheckField = document.getElementById("role-check-field");
-
+const doctorFields = document.getElementById("doctor-fields");
+const checkbox = document.getElementById("role-checkbox");
+var roleCheckField = document.getElementById("role-checkbox");
 
 function showDoctorFields(checkbox)
 {
     if (roleCheckField.checked == true)
     {
-        medicalLicenseField.classList.remove("hideMenu");
-        specialtyField.classList.remove("hideMenu");
-        clinicAddressField.classList.remove("hideMenu");
+        doctorFields.style.display = "block";
         checkbox.value = 'doctor'
     }
     else
     {
-        medicalLicenseField.classList.add("hideMenu");
-        specialtyField.classList.add("hideMenu");
-        clinicAddressField.classList.add("hideMenu");
+        doctorFields.style.display = "none";
         checkbox.value = 'patient'
     }
 }
