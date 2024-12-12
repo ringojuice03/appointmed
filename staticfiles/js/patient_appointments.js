d46bbb0493072a3f8a99886cfc6890da2cfbb9b5
ringojuice03 <kanecorts@gmail.com>
2024-12-12 13:54:48 +0800

    collect static

--- staticfiles/js/patient_appointments.js
+++ staticfiles/js/patient_appointments.js
@@ -1,3 +1,29 @@
+/*user menu*/
+const notificationMenu = document.getElementById("notification-menu");
+const userMenu = document.getElementById("user-menu");
+
+function showNotificationMenu()
+{
+  if (notificationMenu.classList.contains('hideMenu'))
+  {
+    notificationMenu.classList.remove('hideMenu');
+    userMenu.classList.add('hideMenu');
+  }
+  else
+    notificationMenu.classList.add('hideMenu');
+}
+
+function showUserMenu()
+{
+  if (userMenu.classList.contains('hideMenu'))
+  {
+    userMenu.classList.remove('hideMenu');
+    notificationMenu.classList.add('hideMenu');
+  }
+  else
+    userMenu.classList.add('hideMenu');
+}
+
 const expandButtons = document.querySelectorAll('.expand-btn');
 
 expandButtons.forEach((button) => {
